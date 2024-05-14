966a94fb48c73c0f840b60b68ca17b9250b92a13
Elise <elise.maaiza33@gmail.com>
2024-05-14 15:28:43 +0200

    🚧 Ajout slider

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 1500,
-        disableOnInteraction: false
-    },
-});
-
 const images = document.querySelectorAll('.zoomIn');
 const windowWidth = window.innerWidth;
 
