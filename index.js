7278d03e4faf262e5bb317ce0fd99a743bb5611d
Elise <elise.maaiza33@gmail.com>
2024-05-21 19:26:47 +0200

    🚧

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// const images = document.querySelectorAll('.zoomIn');
-// const windowWidth = window.innerWidth;
+const contactForm = document.getElementById('form-contact');
+const emailInput = document.getElementById('floatingInput');
+const msg = document.getElementById('floatingTextarea');
 
-// images.forEach((img) => {
-//     img.addEventListener('click', () => {
-//         img.classList.toggle('zoom');
-//     })
-// })
-
-// function arrowDirection() {
-//     const screenWidth = window.innerWidth;
-
-//     if (screenWidth >= 895) {
-//         changeArrow.innerHTML = "&#8594;";
-//     } else {
-//         changeArrow.innerHTML = "&#8595;";
-//     }
-// }
-
-// window.addEventListener('resize', arrowDirection);
-// window.addEventListener('load', arrowDirection);
+contactForm.addEventListener('submit', (e) => {
+   e.preventDefault;
+   emailInput.value = "";
+   msg.value = "";
+})
