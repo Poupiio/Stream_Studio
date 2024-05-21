0bf9994fb58c8c2af25e5501f37d85467241bdf0
Elise <elise.maaiza33@gmail.com>
2024-05-21 20:16:18 +0200

    🚧

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const contactForm = document.getElementById('form-contact');
 const basicMail = document.getElementById('basic-email');
 const user = document.getElementById('user-name');
 const payPalMail = document.getElementById('paypal-email');
+const tel = document.getElementById('tel');
+const msgSubject = document.getElementById('msg-subject');
 const msg = document.getElementById('floatingTextarea');
 
 contactForm.addEventListener('submit', (e) => {
@@ -9,5 +11,7 @@ contactForm.addEventListener('submit', (e) => {
    basicMail.value = "";
    user.value = "";
    payPalMail.value = "";
+   tel.value = "";
+   msgSubject.value = "12 mois";
    msg.value = "";
 })
