1f14aceac3386ca0e66307c5c497d7e847f28ac1
Elise <elise.maaiza33@gmail.com>
2024-05-25 15:15:31 +0200

    🚧 Form ok

--- index.js
+++ index.js
@@ -6,12 +6,44 @@ const tel = document.getElementById('tel');
 const msgSubject = document.getElementById('msg-subject');
 const msg = document.getElementById('floatingTextarea');
 
-contactForm.addEventListener('submit', (e) => {
-   e.preventDefault;
-   basicMail.value = "";
-   user.value = "";
-   payPalMail.value = "";
-   tel.value = "";
-   msgSubject.value = "12 mois";
-   msg.value = "";
-})
+// contactForm.addEventListener('submit', (e) => {
+//    e.preventDefault;
+//    basicMail.value = "";
+//    user.value = "";
+//    payPalMail.value = "";
+//    tel.value = "";
+//    msgSubject.value = "12 mois";
+//    msg.value = "";
+// })
+
+async function handleSubmit(e) {
+   e.preventDefault();
+
+   let status = document.getElementById("my-form-status");
+   let data = new FormData(e.target);
+
+   fetch(e.target.action, {
+      method: contactForm.method,
+      body: data,
+      headers: {
+         'Accept': 'application/json'
+      }
+   })
+   .then(response => {
+      if (response.ok) {
+         status.innerHTML = `Votre message a bien été envoyé ! <i class="fa-regular fa-face-smile" style="color: #ff0000;"></i>`;
+         contactForm.reset();
+      } else {
+         response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+               status.innerHTML = "Oops! Une erreur s'est produite lors de l'envoi du formulaire !"
+            }
+         })
+      }
+   }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+   });
+};
+contactForm.addEventListener("submit", handleSubmit);
